f9953c74eff6b3bf61597bf307c5869da54bb8ec
jakerydont <jakerydont@gmail.com>
2024-10-02 16:31:53 -0400

    Pace

---
+++ Pace.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace DestinyTrailDotNet
+{
+    public class Pace
+    {
+        public required string Name { get; set; }
+        public int Factor { get; set; }
+    }
+
+
+}
